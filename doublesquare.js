cc7322b32c9183d30707cb65b71aab465f91a192
shabari00 <shabarigirishags.59@gmail.com>
2022-09-12 00:16:55 +0530

    first commit

---
+++ doublesquare.js
@@ -0,0 +1,15 @@
+/*
+Double Square
+Write a function doubleOrSquare, which takes an integer as its argument. If the argument is positive, it should return twice its value, otherwise it should return its square.
+
+You don't need to worry about taking input or printing output. Given template will take care of it. You only have to implement the function doubleOrSquare.
+
+Example
+Input: 5
+
+Output: 10
+
+Input: -5
+
+Output: 25
+*/
